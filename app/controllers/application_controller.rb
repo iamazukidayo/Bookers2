c4cab801bd61b8365140020b7e2ee0b40408eeb8
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-05-12 10:22:46 +0000

    thard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
 
-
+ def after_sign_out_path_for(resourde)
+   root_path
+ end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "homes#top"
+devise_for :users
+
+resources :books, only: [:new, :create, :index, :show, :edit]
+get "/homes/about" => "homes#about", as:"about"
 end
